bba13c262b789da73211f222a3f2df3369d8dac9
Łukasz Oleś <lukaszoles@gmail.com>
2015-07-06 12:47:17 +0200

    Remove hardcoded pin numbers
    Now many pin confiuguration may be created and used

--- picar/__init__.py
+++ picar/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: UTF-8 -*-
 import time
 
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 from flask.ext.uwsgi_websocket import WebSocket
 
 from ext import Platform
@@ -14,20 +14,15 @@ if Platform.pi_version() is None:
 
 from picar import car
 from picar import fpv
+from picar import db as picar_db
 
 
 app = Flask(__name__)
 ws = WebSocket(app)
+db = picar_db.DB()
 
-
-CAR = car.Car(left_pin=24,
-              right_pin=23,
-              forward_pin=15,
-              backward_pin=14,
-              enable_moving=18,
-              enable_turning=25)
-FPV = fpv.FPV(horizontal_pin=8,
-              vertical_pin=7)
+CAR = None
+FPV = None
 
 
 def car_module(msg):
@@ -71,10 +66,48 @@ MODULES = {'car' : car_module,
            'fpv' : fpv_module,
           }
 
-
 @app.route('/')
 def index():
-    return render_template('index.html')
+    controls = db.get('profiles', {})
+    return render_template('index.html', controls=controls)
+
+@app.route('/create', methods=['POST'])
+def create():
+    global CAR, FPV
+    form = dict(request.form)
+    name = form['name'][0]
+    del form['name']
+    for key in form:
+        form[key] = int(form[key][0])
+    CAR = car.Car(**get_pins_for_module(car.Car, form))
+    FPV = fpv.FPV(**get_pins_for_module(fpv.FPV, form))
+    print('Using configuration: {0}'.format(form))
+    controls = db.get('profiles', {})
+    controls[name] = form
+    db.set('controls', controls)
+    return redirect(url_for('profiles'))
+
+@app.route('/configure/<name>')
+def configure(name):
+    global CAR, FPV
+    profiles = db.get('profiles')
+    if not profiles:
+        return redirect(url_for('index'))
+    profile = profiles.get(name, None)
+    if not profile:
+        return redirect(url_for('index'))
+    CAR = car.Car(**get_pins_for_module(car.Car, profile))
+    FPV = fpv.FPV(**get_pins_for_module(fpv.FPV, profile))
+    print('Using configuration: {0}'.format(profile))
+    return redirect(url_for('control'))
+
+@app.route('/control')
+def control():
+    global CAR, FPV
+    if CAR is None or FPV is None:
+        return redirect(url_for('index'))
+    return render_template('control.html')
+
 
 @ws.route('/control-panel')
 def control_panel(ws):
@@ -94,3 +127,11 @@ def control_panel(ws):
                     module('stop')
             else:
                 ws.send("[%s] Unknown message: %s" % (time.time(), msg))
+
+
+def get_pins_for_module(module, pins):
+    selected = {}
+    for pin in pins:
+        if pin in module.PINS:
+            selected[pin] = pins[pin]
+    return selected
